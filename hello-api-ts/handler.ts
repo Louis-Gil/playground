9a784e0c9cfb2d70841a4dab0772f17d3119ec90
Louis-Gil <lotusnate@gmail.com>
2023-09-28 12:59:31 +0900

    feat : Add log message

--- hello-api-ts/handler.ts
+++ hello-api-ts/handler.ts
@@ -3,13 +3,13 @@ import "source-map-support/register"
 
 export const hello: APIGatewayProxyHandlerV2 = async (event) => {
   if (!event.queryStringParameters || !event.queryStringParameters.name) {
-    return { statusCode: 404, body: "Not Found" };
+    console.error('Not Found', 404)
+    return { statusCode: 404, body: 'Not Found' };
   }
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${event.queryStringParameters.name}!`,
-  };
+  const message = `Hello, ${event.queryStringParameters.name}!!`;
+  console.info(message);
+  return { message };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
