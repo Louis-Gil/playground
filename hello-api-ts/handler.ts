ee06bc4ce37ec9e8f3773240c81ce5c9f0cc1bca
Louis-Gil <lotusnate@gmail.com>
2023-09-28 13:11:16 +0900

    style : Update file with prettier

--- hello-api-ts/handler.ts
+++ hello-api-ts/handler.ts
@@ -1,16 +1,16 @@
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
-import "source-map-support/register"
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import 'source-map-support/register';
 
 export const hello: APIGatewayProxyHandlerV2 = async (event) => {
-  if (!event.queryStringParameters || !event.queryStringParameters.name) {
-    console.error('Not Found', 404)
-    return { statusCode: 404, body: 'Not Found' };
-  }
+	if (!event.queryStringParameters || !event.queryStringParameters.name) {
+		console.error('Not Found', 404);
+		return { statusCode: 404, body: 'Not Found' };
+	}
 
-  const message = `Hello, ${event.queryStringParameters.name}!!`;
-  console.info(message);
-  return { message };
+	const message = `Hello, ${event.queryStringParameters.name}!`;
+	console.info(message);
+	return { message };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+	// Use this code if you don't use the http event with the LAMBDA-PROXY integration
+	// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
