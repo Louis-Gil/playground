77826da2c3d159be030ff62d8538531c52c8d3cc
Louis-Gil <lotusnate@gmail.com>
2023-10-07 22:40:25 +0900

    feat : Add center text style

--- flutter_guild/roll_dice/lib/main.dart
+++ flutter_guild/roll_dice/lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-          body: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.blue, Color.fromARGB(255, 87, 172, 241)],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: const Center(child: Text('Hello, World!'))))));
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Color.fromARGB(255, 87, 172, 241)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
