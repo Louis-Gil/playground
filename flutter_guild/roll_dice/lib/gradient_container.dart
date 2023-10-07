559626f6af56675682c9be5444690d74c03f90be
Louis-Gil <lotusnate@gmail.com>
2023-10-08 01:04:25 +0900

    feat : Add dice_roller to change state

--- flutter_guild/roll_dice/lib/gradient_container.dart
+++ flutter_guild/roll_dice/lib/gradient_container.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice/dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
-
-void rollDice() {}
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
@@ -24,24 +24,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-1.png', width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  // padding: const EdgeInsets.all(16),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 20,
-                  )),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
