29dda9315ac81b912917b602e4b904738a23c158
Louis-Gil <lotusnate@gmail.com>
2023-10-07 23:44:24 +0900

    feat : Update split styled_text

--- flutter_guild/roll_dice/lib/gradient_container.dart
+++ flutter_guild/roll_dice/lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice/styled_text.dart';
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -14,10 +16,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
+        child: StyledText(),
       ),
     );
   }
