2800f4f30ce7b3c0dc2d519298924566970cedc2
Louis-Gil <lotusnate@gmail.com>
2023-10-07 22:29:09 +0900

    feat : Add background color gradient

--- flutter_guild/roll_dice/lib/main.dart
+++ flutter_guild/roll_dice/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello, World!')))));
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue, Color.fromARGB(255, 87, 172, 241)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: const Center(child: Text('Hello, World!'))))));
 }
