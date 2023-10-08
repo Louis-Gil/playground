84ca13001ba98c2f1ce079ac04fe56b1ed0c3ac6
Louis-Gil <lotusnate@gmail.com>
2023-10-08 21:20:42 +0900

    style : Update quiz text font, align

--- flutter_guild/quiz/lib/quiz_screen.dart
+++ flutter_guild/quiz/lib/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:quiz/answer_button.dart';
 import 'package:quiz/data/questions.dart';
@@ -35,7 +36,11 @@ class _QuizScreenState extends State<QuizScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
