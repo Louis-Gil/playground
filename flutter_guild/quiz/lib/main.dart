87ee67e2f3636ad55af3faf2b3281a77cd28d79b
Louis-Gil <lotusnate@gmail.com>
2023-10-08 13:21:05 +0900

    style : Update start screen page

--- flutter_guild/quiz/lib/main.dart
+++ flutter_guild/quiz/lib/main.dart
@@ -3,8 +3,21 @@ import 'package:quiz/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(body: StartScreen()),
+    MaterialApp(
+      home: Scaffold(
+          body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue,
+              Colors.purple,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: const StartScreen(),
+      )),
     ),
   );
 }
