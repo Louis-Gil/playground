296b9bca3d37f95a5f0bfad33cffc16bce55ab4d
Louis-Gil <lotusnate@gmail.com>
2023-10-08 13:30:26 +0900

    style : start button icon

--- flutter_guild/quiz/lib/start_screen.dart
+++ flutter_guild/quiz/lib/start_screen.dart
@@ -6,30 +6,32 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 300,
-        ),
-        const SizedBox(height: 50),
-        const Text(
-          'Learn Flutter thr fun way!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
           ),
-        ),
-        const SizedBox(height: 30),
-        OutlinedButton(
-          onPressed: () {},
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white,
+          const SizedBox(height: 50),
+          const Text(
+            'Learn Flutter thr fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
           ),
-          child: const Text('Start Quiz'),
-        )
-      ],
-    ));
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
   }
 }
