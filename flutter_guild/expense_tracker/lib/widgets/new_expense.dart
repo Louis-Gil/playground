22dcd70f9864941cc9fbb914744a92d537b9614d
Louis-Gil <lotusnate@gmail.com>
2023-10-09 15:54:41 +0900

    feat : Add validation new expense

--- flutter_guild/expense_tracker/lib/widgets/new_expense.dart
+++ flutter_guild/expense_tracker/lib/widgets/new_expense.dart
@@ -30,6 +30,32 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -116,11 +142,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                  // print(_enteredTitle);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expnse'),
               ),
             ],
