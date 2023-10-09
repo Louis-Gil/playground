090c9f6f1b9441c39ad129fad8dfe7caf2b3e4db
Louis-Gil <lotusnate@gmail.com>
2023-10-09 12:06:02 +0900

    feat : Add expenses list at expenses column

--- flutter_guild/expense_tracker/lib/expenses.dart
+++ flutter_guild/expense_tracker/lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,13 +13,30 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 10.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 12.35,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list'),
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
