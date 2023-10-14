6b23f42ce4253ab3a222d2d81e317728aad120e4
Louis-Gil <lotusnate@gmail.com>
2023-10-14 19:27:42 +0900

    feat : Add meal detail screen

--- flutter_guild/meal/lib/screens/meals.dart
+++ flutter_guild/meal/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal/models/meal.dart';
+import 'package:meal/screens/meal_details.dart';
 import 'package:meal/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -12,11 +13,26 @@ class MealsScreen extends StatelessWidget {
   final String title;
   final List<Meal> meals;
 
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
+        onSelectMeal: (meal) {
+          selectMeal(context, meal);
+        },
+      ),
     );
 
     if (meals.isEmpty) {
