40955228b54b39826bda16995ea121ee004dff9a
Louis-Gil <lotusnate@gmail.com>
2023-10-16 22:53:40 +0900

    feat : Add error handling when did not fetch db

--- flutter_guild/shopping_list/lib/widgets/grocery_list.dart
+++ flutter_guild/shopping_list/lib/widgets/grocery_list.dart
@@ -20,6 +20,7 @@ class _GroceryListState extends State<GroceryList> {
 
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -30,6 +31,13 @@ class _GroceryListState extends State<GroceryList> {
   void _loadItems() async {
     final url = Uri.https(firebaseDbKey, firebaseDbTable);
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Could not fetch items.';
+      });
+    }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -102,6 +110,12 @@ class _GroceryListState extends State<GroceryList> {
             ),
           ),
         ),
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
       );
     }
 
