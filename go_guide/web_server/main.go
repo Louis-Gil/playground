7f69a584a5e97c006910fa2f399ada30eb0f1fea
Louis-Gil <lotusnate@gmail.com>
2023-10-21 17:18:21 +0900

    feat : Add bar url response id

--- go_guide/web_server/main.go
+++ go_guide/web_server/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World")
 	})
+	http.HandleFunc("/bar", barHandler)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	name := values.Get("name")
+	if name == "" {
+		name = "World"
+	}
+	id, _ := strconv.Atoi(values.Get("id"))
+	fmt.Fprintf(w, "Hello %s! id:%d", name, id)
 }
