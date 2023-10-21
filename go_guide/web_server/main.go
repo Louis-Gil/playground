afeca63e3d3647b1b04176d96d90565b511776ca
Louis-Gil <lotusnate@gmail.com>
2023-10-21 18:06:48 +0900

    feat : Add return student json with test

--- go_guide/web_server/main.go
+++ go_guide/web_server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -10,12 +11,19 @@ func main () {
 	http.ListenAndServe(":8080", MakeWebHandler())
 }
 
+type Student struct {
+	Name string
+	Age int
+	Score int
+}
+
 func MakeWebHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World")
 	})
 	mux.HandleFunc("/bar", barHandler)
+	mux.HandleFunc("/student", StudentHandler)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	return mux
@@ -29,4 +37,12 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	id, _ := strconv.Atoi(values.Get("id"))
 	fmt.Fprintf(w, "Hello %s! id:%d", name, id)
+}
+
+func StudentHandler(w http.ResponseWriter, r *http.Request) {
+	var student = Student{"duli", 20, 90}
+	data, _ := json.Marshal(student)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
 }
--- go_guide/web_server/main_test.go
+++ go_guide/web_server/main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -32,4 +34,21 @@ func TestBarHandler(t *testing.T) {
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := io.ReadAll(res.Body)
 	assert.Equal("Hello duli! id:3", string(data))
+}
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/student", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	student := new(Student)
+	err := json.NewDecoder(res.Body).Decode(student)
+	assert.Nil(err)
+	assert.Equal("duli", student.Name)
+	assert.Equal(20, student.Age)
+	assert.Equal(90, student.Score)
 }
