3739d0925e7b3f653bc857720b47c8041b9fcdec
Louis-Gil <lotusnate@gmail.com>
2023-12-05 22:25:36 +0900

    feat : Add props

--- react_guide/src/index.js
+++ react_guide/src/index.js
@@ -9,8 +9,8 @@ root.render(
 		<div className="container">
 			<div className="row">
 				<div className="col">
-					<AppClass />
-					<HelloWorld />
+					<AppClass msg="hello world!" />
+					<HelloWorld msg="hello again!"/>
 				</div>
 			</div>
 		</div>
