bda3aaad7a49cf112c4d3e2cf0280b4b7a4670df
Louis-Gil <lotusnate@gmail.com>
2023-12-05 23:58:24 +0900

    feat : Add first name input box

--- react_guide/src/App.js
+++ react_guide/src/App.js
@@ -5,6 +5,10 @@ function HelloWorld(props) {
 	const [isTrue, setIsTrue] = useState(false);
 	const [crowd, setCrowd] = useState([]);
 
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [dob, setDob] = useState('');
+
 	const toggleTrue = () => {
 		if (isTrue) {
 			setIsTrue(false);
@@ -53,6 +57,23 @@ function HelloWorld(props) {
 			<a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>
 				Toggle isTrue
 			</a>
+			<hr />
+			<form autoComplete="off">
+				<div className="mb-3">
+					<label className="form-label">First Name</label>
+					<input
+						type="text"
+						name="firstName"
+						id="firstName"
+						autoComplete="firstNameNew"
+						className="form-control"
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
+				</div>
+			</form>
+      <div>
+        First Name: {firstName}
+      </div>
 			<hr />
 			<h3>people</h3>
 			<ul className="list-group">
