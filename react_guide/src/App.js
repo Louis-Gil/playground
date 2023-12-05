6f8f935cb1f593c1a7eb441cf8d0509a949f661b
Louis-Gil <lotusnate@gmail.com>
2023-12-05 22:52:59 +0900

    feat : Add useEffect

--- react_guide/src/App.js
+++ react_guide/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
 
 function HelloWorld(props) {
 	const [isTrue, setIsTrue] = useState(false);
+	const [crowd, setCrowd] = useState([]);
 
 	const toggleTrue = () => {
 		if (isTrue) {
@@ -11,6 +12,32 @@ function HelloWorld(props) {
 		}
 		setIsTrue(true);
 	};
+
+	useEffect(() => {
+		console.log('HelloWorld useEffect');
+
+		let people = [
+			{
+				id: 1,
+				firstName: 'Leland',
+				lastName: 'Kwong',
+				dob: '1980-01-01',
+			},
+			{
+				id: 2,
+				firstName: 'John',
+				lastName: 'Doe',
+				dob: '1980-01-01',
+			},
+			{
+				id: 3,
+				firstName: 'Jane',
+				lastName: 'Doe',
+				dob: '1980-01-01',
+			},
+		];
+		setCrowd(people);
+	}, []);
 
 	return (
 		<Fragment>
@@ -26,6 +53,15 @@ function HelloWorld(props) {
 			<a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>
 				Toggle isTrue
 			</a>
+			<hr />
+			<h3>people</h3>
+			<ul className="list-group">
+				{crowd.map((m) => (
+					<li key={m.id} className="list-group-item">
+						{m.firstName} {m.lastName}
+					</li>
+				))}
+			</ul>
 		</Fragment>
 	);
 }
