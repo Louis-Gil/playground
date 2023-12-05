ca5e732fe00b055ae1fa136f132304b184dfad97
Louis-Gil <lotusnate@gmail.com>
2023-12-05 22:19:40 +0900

    feat : Add bootstrap

--- react_guide/src/index.js
+++ react_guide/src/index.js
@@ -5,8 +5,14 @@ import HelloWorld from './HelloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AppClass />
-    <HelloWorld />
-  </React.StrictMode>
+	<React.StrictMode>
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<AppClass />
+					<HelloWorld />
+				</div>
+			</div>
+		</div>
+	</React.StrictMode>
 );
