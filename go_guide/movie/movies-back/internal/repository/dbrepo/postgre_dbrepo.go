f309ce59e6c891fc29a797e18b678fb8187d1363
Louis-Gil <lotusnate@gmail.com>
2023-12-17 01:21:24 +0900

    feat : Add refresh func

--- go_guide/movie/movies-back/internal/repository/dbrepo/postgre_dbrepo.go
+++ go_guide/movie/movies-back/internal/repository/dbrepo/postgre_dbrepo.go
@@ -63,10 +63,10 @@ func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 }
 
 func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
-  ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
 
-  query := `
+	query := `
     SELECT
       id, email, first_name, last_name, password, created_at, updated_at
     FROM
@@ -75,22 +75,55 @@ func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
       email = $1
   `
 
-  var user models.User
-  row := m.DB.QueryRowContext(ctx, query, email)
-  
-  err := row.Scan(
-    &user.ID,
-    &user.Email,
-    &user.FirstName,
-    &user.LastName,
-    &user.Password,
-    &user.CreatedAt,
-    &user.UpdatedAt,
-  )
+	var user models.User
+	row := m.DB.QueryRowContext(ctx, query, email)
 
-  if err != nil {
-    return nil, err
-  }
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
 
-  return &user, nil
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (m *PostgresDBRepo) GetUserById(id int) (*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+    SELECT
+      id, email, first_name, last_name, password, created_at, updated_at
+    FROM
+      users
+    WHERE
+      id = $1
+  `
+
+	var user models.User
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
