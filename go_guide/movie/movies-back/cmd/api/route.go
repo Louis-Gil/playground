a3b501bea05d120b8753e0f5b7d556c62d22894f
Louis-Gil <lotusnate@gmail.com>
2023-12-16 23:39:57 +0900

    feat : Update authenticate add read json

--- go_guide/movie/movies-back/cmd/api/route.go
+++ go_guide/movie/movies-back/cmd/api/route.go
@@ -15,7 +15,7 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 
-	mux.Get("/authenticate", app.authenicate)
+	mux.Get("/authenticate", app.authenticate)
 
 	mux.Get("/movies", app.AllMovies)
 
