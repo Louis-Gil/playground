ec7802be94d025b11fd80318f805d1bdfc92d8be
Louis-Gil <lotusnate@gmail.com>
2023-12-18 22:28:36 +0900

    feat : Add front checkbox form

---
+++ go_guide/movie/movies-front/src/components/form/CheckBox.js
@@ -0,0 +1,20 @@
+const CheckBox = (props) => {
+	return (
+		<div className="form-check">
+			<input
+				className="form-check-input"
+				id={props.name}
+				name={props.name}
+				type="checkbox"
+				value={props.value}
+				onChange={props.onChange}
+				checked={props.checked}
+			/>
+			<label className="form-check-label" htmlFor={props.name}>
+				{props.title}
+			</label>
+		</div>
+	);
+};
+
+export default CheckBox;
