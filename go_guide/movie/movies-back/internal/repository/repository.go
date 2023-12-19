8cbe9a1e65b3392cfa0e5c058db7643797a3f8b0
Louis-Gil <lotusnate@gmail.com>
2023-12-19 22:46:59 +0900

    feat : Add get oneMovie func

--- go_guide/movie/movies-back/internal/repository/repository.go
+++ go_guide/movie/movies-back/internal/repository/repository.go
@@ -8,6 +8,9 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
-  GetUserByEmail(email string) (*models.User, error)
-  GetUserById(id int) (*models.User, error)
+	GetUserByEmail(email string) (*models.User, error)
+	GetUserById(id int) (*models.User, error)
+
+	OneMovie(id int) (*models.Movie, error)
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 }
