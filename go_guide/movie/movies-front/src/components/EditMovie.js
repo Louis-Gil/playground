73b70b3e20ab81268ca1e034be2180b20f66add4
Louis-Gil <lotusnate@gmail.com>
2023-12-20 23:52:15 +0900

    feat : Add front show edit genre

--- go_guide/movie/movies-front/src/components/EditMovie.js
+++ go_guide/movie/movies-front/src/components/EditMovie.js
@@ -3,6 +3,7 @@ import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
 import Input from './form/Input';
 import Select from './form/Select';
 import TextArea from './form/TextArea';
+import Checkbox from './form/CheckBox';
 
 const EditMovie = () => {
 	const navigate = useNavigate();
@@ -31,16 +32,60 @@ const EditMovie = () => {
 		runtime: '',
 		mpaa_rating: '',
 		description: '',
+		genres: [],
+		genres_array: [Array(13).fill(false)],
 	});
 
 	let { id } = useParams();
+	if (id === undefined) {
+		id = 0;
+	}
 
 	useEffect(() => {
 		if (jwtToken === '') {
 			navigate('/login');
 			return;
 		}
-	}, [jwtToken, navigate]);
+
+		if (id === 0) {
+			// Add Movie
+			setMovie({
+				id: 0,
+				title: '',
+				release_date: '',
+				runtime: '',
+				mpaa_rating: '',
+				description: '',
+				genres: [],
+				genres_array: [Array(13).fill(false)],
+			});
+
+			const headers = new Headers();
+			headers.append('Content-Type', 'application/json');
+
+			const requestOptions = {
+				method: 'GET',
+				headers: headers,
+			};
+
+			fetch(`/genres`, requestOptions)
+				.then((response) => response.json())
+				.then((data) => {
+					const checks = [];
+
+					data.forEach((genre) => {
+						checks.push({ id: genre.id, checked: false, genre: genre.genre });
+					});
+
+					setMovie((m) => ({ ...m, genres: checks, genres_array: [] }));
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			// Edit Movie
+		}
+	}, [id, jwtToken, navigate]);
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
@@ -51,6 +96,13 @@ const EditMovie = () => {
 		let name = event.target.name;
 		setMovie({ ...movie, [name]: value });
 	};
+
+  const handleCheck = (event, position) => {
+    console.log("handleCheck called")
+    console.log("value in handleCheck: " + event.target.value)
+    console.log("checked is", event.target.checked)
+    console.log("position is", position)
+  }
 
 	return (
 		<div>
@@ -104,15 +156,35 @@ const EditMovie = () => {
 					errorMsg={'Please choose a rating'}
 				/>
 
-        <TextArea
-          title={'Description'}
-          name={'description'}
-          value={movie.description}
-          rows={3}
-          onChange={handleChange('description')}
-          errorDiv={hasError('description') ? 'text-danger' : 'd-none'}
-          errorMsg={'Please enter a description'}
-        />
+				<TextArea
+					title={'Description'}
+					name={'description'}
+					value={movie.description}
+					rows={3}
+					onChange={handleChange('description')}
+					errorDiv={hasError('description') ? 'text-danger' : 'd-none'}
+					errorMsg={'Please enter a description'}
+				/>
+
+				<hr />
+
+				<h3>Genres</h3>
+
+				{movie.genres && movie.genres.length > 0 && (
+					<>
+						{Array.from(movie.genres).map((g, index) => (
+							<Checkbox
+								title={g.genre}
+								name={'genre'}
+								key={index}
+								id={'genre-' + index}
+								onChange={(event) => handleCheck(event, index)}
+								value={g.id}
+								checked={movie.genres[index].checked}
+							/>
+						))}
+					</>
+				)}
 			</form>
 		</div>
 	);
