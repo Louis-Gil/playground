cdab5201469d70a0cb77ac610de508378ecb57e6
Louis-Gil <lotusnate@gmail.com>
2023-12-31 15:41:52 +0900

    test : Update dbrepo apply repo pattern

--- go_guide/test_server/cmd/web/setup_test.go
+++ go_guide/test_server/cmd/web/setup_test.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"log"
 	"os"
-	"test_server/pkg/db"
+	"test_server/pkg/repository/dbrepo"
 	"testing"
 )
 
@@ -13,15 +12,7 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
-	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
-
-	conn, err := app.connectToDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	app.DB = db.PostgresConn{DB: conn}
+	app.DB = &dbrepo.TestDBRepo{}
 
 	os.Exit(m.Run())
 }
